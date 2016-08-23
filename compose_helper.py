6153808b46898c9a80750dcdfbb25a3769080265
Miguel Barrientos <miguelonbf@gmail.com>
2016-08-23 18:10:20 +0200

    Added SSH command

--- compose_helper.py
+++ compose_helper.py
@@ -18,7 +18,7 @@ COMPOSE_CHOICES = (
 )
 
 COMMAND_CHOICES = COMPOSE_CHOICES + (
-    'migrate', 'ssh',
+    'ssh',
 )
 
 DEFAULT_CMD = 'up'
@@ -32,8 +32,8 @@ class ComposeHelper:
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("command", help="Command to run on given app",
                                  default=DEFAULT_CMD, type=str)
-        self.parser.add_argument("--compose", help="Path to docker-compose file")
-        self.parser.add_argument("-s", "--service", dest='service', type=str,
+        self.parser.add_argument("-c", "--compose", dest="compose", help="Path to docker-compose file")
+        self.parser.add_argument("-s", "--service", dest="service", type=str,
                                  help="Compose service to target. Defaults to 'default_service' at the config file")
         self.parser.add_argument("args", nargs=argparse.REMAINDER, help="Additional arguments for running command")
         self.args = self.parser.parse_args()
@@ -47,6 +47,13 @@ class ComposeHelper:
 
         self.project_dir = self.config[self.app].get('project_dir', os.getcwd())
         self.default_service = self.config[self.app].get('default_service', None)
+
+        if self.args.service:
+            self.service = self.args.service
+        elif self.default_service:
+            self.service = self.default_service
+        else:
+            self.service = None
 
     def get_app_path(self, app: str) -> str:
         """
@@ -70,22 +77,42 @@ class ComposeHelper:
             return os.path.join(self.get_app_path(self.app), "docker-compose.yml")
 
     def docker_compose(self, compose_command):
+        """
+        Builds the docker-compose command.
+        :param compose_command: docker-compose command
+        :return:
+        """
         cmd = shlex.split("docker-compose -f {} {}".format(self.get_docker_compose(), compose_command))
 
-        if self.args.service:
-            service = self.args.service
-        elif self.default_service:
-            service = self.default_service
-        else:
-            service = None
-
-        if service:
+        if self.service:
             if compose_command in COMPOSE_SERVICE_SPECIFIC:
-                cmd.append(service)
+                cmd.append(self.service)
                 cmd.extend(self.args.args)
             else:
                 cmd.extend(self.args.args)
-                cmd.append(service)
+                if self.args.service:
+                    # Only appending service if it was explicitly specified on command
+                    cmd.append(self.args.service)
+
+        return cmd
+
+    def ssh(self):
+        """
+        Create a ssh connection on an existing running service.
+        :return:
+        """
+
+        if self.service:
+            cmd = shlex.split("docker-compose -f {} exec {}".format(self.get_docker_compose(), self.service))
+
+            if self.args.args:
+                # Assume a shell binary was provided
+                cmd.extend(self.args.args)
+            else:
+                # Default to /bin/sh shell
+                cmd.append('/bin/sh')
+        else:
+            raise Exception('No service to ssh into.')
 
         return cmd
 
@@ -111,6 +138,7 @@ class ComposeHelper:
         elif cmd in COMMAND_CHOICES:
             command_f = getattr(self, cmd, None)
             shell_commands = command_f()
+            print(shell_commands)
         else:
             return self.not_found()
 
